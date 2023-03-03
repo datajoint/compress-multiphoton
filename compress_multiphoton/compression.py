9b69ba4d293b4b7133b10ddb598a73c66ded36fa
Tolga Dincer <tolgadincer@gmail.com>
2023-03-03 15:58:04 -0600

    refactor compute_quantal_size

--- compress_multiphoton/compression.py
+++ compress_multiphoton/compression.py
@@ -2,57 +2,47 @@ import numpy as np
 from sklearn.linear_model import TheilSenRegressor
 
 
-def compute_quantal_size(scan, max_acceptable_intensity=3000):
-    # Set some params
-    num_frames = scan.shape[2]
-    # min_count = num_frames * 0.1  # pixel values with fewer appearances will be ignored
-    min_count = scan.min()
-    # max_acceptable_intensity = 100000
+def _longest_run(bool_array):
+    step = np.diff(np.int8(bool_array), prepend=0, append=0)
+    on = np.where(step == 1)[0]
+    off = np.where(step == -1)[0]
+    i = np.argmax(off - on)
+    return slice(on[i], off[i])
 
-    # Make sure field is at least 32 bytes (int16 overflows if summed to itself)
-    scan = scan.astype(np.float32, copy=False)
 
-    # Create pixel values at each position in field
-    eps = 1e-4  # needed for np.round to not be biased towards even numbers (0.5 -> 1, 1.5 -> 2, 2.5 -> 3, etc.)
-    pixels = np.round((scan[:, :, :-1] + scan[:, :, 1:]) / 2 + eps)
+def compute_quantal_size(movie):
+    movie = movie.astype(np.int32, copy=False)
+    intensity = (movie[:, :, :-1] + movie[:, :, 1:] + 1) // 2
+    difference = movie[:, :, :-1] - movie[:, :, 1:]
 
-    pixels = pixels.astype(np.int16 if np.max(abs(pixels)) < 2**15 else np.int32)
+    CTS_THRESHOLD = 1000
+    cts = np.bincount(intensity.flatten())
+    cts_slice = _longest_run(cts > CTS_THRESHOLD)
+    cts_slice = slice(max(cts_slice.stop * 20 // 100, cts_slice.start), cts_slice.stop)
 
-    # Compute a good range of pixel values (common, not too bright values)
-    unique_pixels, counts = np.unique(pixels, return_counts=True)
-    min_intensity = min(unique_pixels[counts > min_count])
-    max_intensity = max(unique_pixels[counts > min_count])
-    max_acceptable_intensity = min(max_intensity, max_acceptable_intensity)
-    pixels_mask = np.logical_and(pixels >= min_intensity, pixels <= max_acceptable_intensity)
+    counts = counts[cts_slice]
+    idx = (intensity >= cts_slice.start) & (intensity < cts_slice.stop)
+    variance = (
+        np.bincount(
+            intensity[idx] - cts_slice.start,
+            weights=(np.float32(difference[idx]) ** 2) / 2,
+        )
+        / counts
+    )
 
-    # Select pixels in good range
-    pixels = pixels[pixels_mask]
-    unique_pixels, counts = np.unique(pixels, return_counts=True)
+    intensity_levels = np.arange(cts_slice.start, cts_slice.stop)
 
-    # Compute noise variance
-    variances = ((scan[:, :, :-1] - scan[:, :, 1:]) ** 2 / 2)[pixels_mask]
-    pixels -= min_intensity
-    variance_sum = np.zeros(len(unique_pixels))  # sum of variances per pixel value
-    for i in range(0, len(pixels), int(1e8)):  # chunk it for memory efficiency
-        variance_sum += np.bincount(
-            pixels[i : i + int(1e8)], weights=variances[i : i + int(1e8)], minlength=np.ptp(unique_pixels) + 1
-        )[unique_pixels - min_intensity]
-    unique_variances = variance_sum / counts  # average variance per intensity
-
-    # Compute quantal size (by fitting a linear regressor to predict the variance from intensity)
-    X = unique_pixels.reshape(-1, 1)
-    y = unique_variances
-    model = TheilSenRegressor()  # robust regression
-    model.fit(X, y)
+    model = TheilSenRegressor()
+    model.fit(intensity_levels.reshape(-1, 1), variance)
     quantal_size = model.coef_[0]
     zero_level = -model.intercept_ / model.coef_[0]
 
     output = dict(
         model=model,
-        min_intensity=min_intensity,
-        max_intensity=max_intensity,
-        unique_pixels=unique_pixels,
-        unique_variances=unique_variances,
+        min_intensity=cts_slice.start,
+        max_intensity=cts_slice.stop,
+        unique_intensities=intensity_levels,
+        unique_variances=variance,
         quantal_size=quantal_size,
         zero_level=zero_level,
     )
@@ -74,5 +64,7 @@ def anscombe(frames, a0: float, a1: float, beta: float):
     Returns:
         transformed_frames: _description_
     """
-    transformed_frames = (2.0 / beta * np.sqrt((frames + a0) / a1 + 0.375)).astype(np.int8)
+    transformed_frames = (2.0 / beta * np.sqrt((frames + a0) / a1 + 0.375)).astype(
+        np.int8
+    )
     return transformed_frames
