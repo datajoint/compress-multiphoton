855f05ffcedd99043a008db91a1d18a61784456d
Tolga Dincer <tolgadincer@gmail.com>
2023-03-07 14:14:00 -0600

    adjust analyze.py and example notebook

--- compress_multiphoton/analysis.py
+++ compress_multiphoton/analysis.py
@@ -19,17 +19,17 @@ def timer_func(func):
 def analyze(scan):
     qs = compute_quantal_size(scan)
 
-    print(f"Quantal size: {quantal_size}")
-    print(f"Intercept: {zero_level}")
+    print(f'Quantal size: {qs["quantal_size"]}')
+    print(f'Intercept: {qs["zero_level"]}')
 
-    x = np.arange(len(qs["unique_pixels"]))
-    f = go.Figure(go.Scatter(x=qs["unique_pixels"], y=qs["unique_variances"], mode="markers"))
+    x = np.arange(qs["min_intensity"], qs["max_intensity"])
+    f = go.Figure(go.Scatter(x=x, y=qs["variance"], mode="markers"))
     f.add_trace(go.Scatter(x=x, y=qs["model"].predict(x.reshape(-1, 1))))
     f.update_layout(
         width=700,
         height=700,
         yaxis=dict(title_text="Variance"),
-        xaxis=dict(title_text="Pixel Intensity"),
+        xaxis=dict(title_text="Intensity Levels"),
         showlegend=False,
     )
 
